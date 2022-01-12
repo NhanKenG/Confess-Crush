df384adca81c8eeef41bf1e0e4c475b6b9b7144d
NhanKenG <97569766+NhanKenG@users.noreply.github.com>
2022-01-11 19:36:42 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "Những câu hỏi cho Thịnh",
+    introTitle: 'Thịnh Chó Đỏ',
+    introDesc: `Nhân có vài câu hỏi muốn hỏi Thịnh`,
+    btnIntro: '^^Oki Nhân hỏi đi^^',
+    title: ' Câu hỏi 1 liên quan tới Nhân một chút 🥰',
+    desc: ' Thịnh thấy Nhân có đẹp trai không ',
+    btnYes: 'Nhân không chỉ đẹp trai mà còn hài hước năng động😘 <33',
+    btnNo: 'Nhân xấu lắm :((',
+    question: 'Câu hỏi 2: Trên thế giới hơn 7 tỉ người mà sao  lại yêu Thơ ?',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: ' Thịnh có yêu Thơ đâu tại Thịnh bứ <33333333',
+    mess: ' Nhân biết ngay mà 🥰.  Thịnh bứ vãi lìn 😘😘',
+    messDesc: 'Mà Thơ giờ chỉ xem Thịnh là bạn à! Thôi Thịnh chơi Bê Đê đi! Nhân hỏi xong rồi Bye Thịnh.',
+    btnAccept: 'Ok Bye Nhân Thịnh đi chơi Bê Đê đây <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
